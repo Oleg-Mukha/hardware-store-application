8fd8498025061f30350f596760f5f9bce030a7ec
Oleg <oleg.mukha.2002@gmail.com>
2023-06-17 23:03:37 +0300

    Added data class for advertisement.

---
+++ app/src/main/java/com/example/hardware_store_app/data/Advert.kt
@@ -0,0 +1,8 @@
+package com.example.hardware_store_app.data
+
+data class Advert(
+    val id: Int? = 0,
+    val advertName: String? = null,
+    val advertDescription: String? = null,
+    val image: Int? = null
+)
