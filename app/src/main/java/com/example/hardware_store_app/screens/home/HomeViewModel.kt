755adcd38dc82c42a609e46fc7a5e8b23c3f4f6a
Oleg <oleg.mukha.2002@gmail.com>
2023-06-18 15:36:26 +0300

    Test data for adverts and categories added.

--- app/src/main/java/com/example/hardware_store_app/screens/home/HomeViewModel.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/home/HomeViewModel.kt
@@ -1,7 +1,26 @@
 package com.example.hardware_store_app.screens.home
 
 import androidx.lifecycle.ViewModel
+import com.example.hardware_store_app.R
+import com.example.hardware_store_app.data.Advert
+import com.example.hardware_store_app.data.Product
 
 class HomeViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val listOfAds = listOf(
+        Advert(
+            advertName = "Advert caption",
+            advertDescription = "Advert description, description",
+            advertImage = R.drawable.ic_launcher_foreground
+        ),
+        Advert(
+            advertName = "Advert caption",
+            advertDescription = "Advert description, description"
+        ),
+    )
+
+    val listOfCategories = listOf(
+        Product(name = "Construction Materials", image = R.drawable.ic_launcher_foreground),
+        Product(name = "Paints and varnishes", image = R.drawable.ic_launcher_foreground),
+        Product(name = "Electrical goods", image = R.drawable.ic_launcher_foreground)
+    )
 }
