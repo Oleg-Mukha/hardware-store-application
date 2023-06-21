9930db38ca7b4cb39cc18af018d8d27967a03ac0
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-21 15:15:05 +0300

    ActionHandler added.

---
+++ app/src/main/java/com/example/hardware_store_app/ActionHandler.kt
@@ -0,0 +1,11 @@
+package com.example.hardware_store_app
+
+import com.example.hardware_store_app.db.data.Goods
+
+interface ActionHandler {
+    fun onCategoryClick(category: String)
+}
+
+interface OnItemClick{
+    fun onItemClick(id: String)
+}
