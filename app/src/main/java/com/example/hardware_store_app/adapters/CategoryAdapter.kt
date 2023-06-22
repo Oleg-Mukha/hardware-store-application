6996cea494eadaff37452e2538968b2b59614892
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 11:32:38 +0300

    Category card adapter updated.

--- app/src/main/java/com/example/hardware_store_app/adapters/CategoryAdapter.kt
+++ app/src/main/java/com/example/hardware_store_app/adapters/CategoryAdapter.kt
@@ -5,21 +5,32 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
+import com.example.hardware_store_app.ActionHandler
 import com.example.hardware_store_app.R
 import com.example.hardware_store_app.data.Product
+import com.example.hardware_store_app.databinding.CategoryCardBinding
 
-class CategoryAdapter(private var categoryList: List<Product>) :
+class CategoryAdapter(private var categoryList: List<Product>, val actionHandler: ActionHandler) :
     RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val categoryCard: ConstraintLayout = view.findViewById(R.id.cl_category_card)
         val categoryImage: ImageView = view.findViewById(R.id.iv_category)
         val categoryName: TextView = view.findViewById(R.id.tv_category)
+        fun getCategory(): String = categoryName.text.toString()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view =
             LayoutInflater.from(parent.context).inflate(R.layout.category_card, parent, false)
+        val holder = ViewHolder(view)
+
+        holder.categoryCard.setOnClickListener {
+            actionHandler.onCategoryClick(holder.getCategory())
+        }
+
         return ViewHolder(view)
     }
 
