657f7b29aa684965f761be5420c0bf1f715b6490
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:40:06 +0300

    New interaction features added.

--- app/src/main/java/com/example/hardware_store_app/screens/details/DetailsViewModel.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/details/DetailsViewModel.kt
@@ -3,7 +3,9 @@ package com.example.hardware_store_app.screens.details
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.hardware_store_app.db.data.Cart
 import com.example.hardware_store_app.db.data.Goods
+import com.example.hardware_store_app.db.data.Like
 import com.example.hardware_store_app.db.repository.LocalProductRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -15,15 +17,42 @@ class DetailsViewModel @Inject constructor(
 ) : ViewModel() {
 
     val productItem = MutableLiveData<Goods>()
+    val isLikedItem = MutableLiveData<Boolean>()
 
     fun getProductById(id: String) {
         viewModelScope.launch {
             productItem.value = productLocalRepository.getProductById(id)
         }
     }
-    val product = Goods(
-        productItem.value?.id,
-        productItem.value?.name,
-        productItem.value?.price?.toInt()
-    )
+
+    fun isLiked(id: String) {
+        viewModelScope.launch {
+            isLikedItem.value = productLocalRepository.isLiked(id)
+        }
+    }
+
+    fun insertProduct(like: Like) {
+        viewModelScope.launch {
+            productLocalRepository.insertLikeItem(like)
+        }
+    }
+
+    fun deleteProduct(id: String) {
+        viewModelScope.launch {
+            productLocalRepository.deleteLikeItem(id)
+        }
+    }
+
+    fun addProductToCart() {
+        val cart = Cart(
+            productItem.value?.id,
+            productItem.value?.name,
+            productItem.value?.image,
+            productItem.value?.price,
+            productItem.value?.category
+        )
+        viewModelScope.launch {
+            productLocalRepository.addProductToCart(cart)
+        }
+    }
 }
