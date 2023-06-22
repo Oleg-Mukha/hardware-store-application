2e08c1a86b3b186c9de292d26363ce9cee1dcafc
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:37:31 +0300

    Cart view model updated.

--- app/src/main/java/com/example/hardware_store_app/screens/cart/CartViewModel.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/cart/CartViewModel.kt
@@ -1,7 +1,29 @@
 package com.example.hardware_store_app.screens.cart
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hardware_store_app.db.data.Cart
+import com.example.hardware_store_app.db.repository.LocalProductRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class CartViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class CartViewModel @Inject constructor(
+    private val localProductRepository: LocalProductRepository
+) : ViewModel() {
+    val cartList = MutableLiveData<MutableList<Cart>>()
+
+    fun getProductsFromCart() {
+        viewModelScope.launch {
+            cartList.value = localProductRepository.getProductsFromCart()
+        }
+    }
+
+    fun deleteProductFromCart(id: Int) {
+        viewModelScope.launch {
+            localProductRepository.deleteProductFromCart(id)
+    }
+}
 }
