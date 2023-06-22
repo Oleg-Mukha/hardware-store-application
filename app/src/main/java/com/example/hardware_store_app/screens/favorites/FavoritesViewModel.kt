e975decbd954e5d4879013cdbef22e5845e06305
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:41:21 +0300

    New features implemented.

--- app/src/main/java/com/example/hardware_store_app/screens/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/favorites/FavoritesViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.hardware_store_app.screens.favorites
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.hardware_store_app.db.data.Goods
+import com.example.hardware_store_app.db.repository.LocalProductRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    private val localProductRepository: LocalProductRepository
+) : ViewModel() {
 
-class FavoritesViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    val listOfLikedItems = MutableLiveData<List<Goods>>()
+    private val favoritesList = mutableListOf<Goods>()
+
+    fun getLikedItems() {
+        viewModelScope.launch {
+            localProductRepository.getAllLikeItems().forEach {
+                favoritesList.add(localProductRepository.getProductById(it.idProduct))
+            }
+            listOfLikedItems.value = favoritesList
+        }
+    }
 }
