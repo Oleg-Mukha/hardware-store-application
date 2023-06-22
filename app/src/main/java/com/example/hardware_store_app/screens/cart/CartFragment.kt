79f416b7b0463e033eb5ce8aa8cc48bd7b1cf90d
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:37:20 +0300

    Cart fragment updated.

--- app/src/main/java/com/example/hardware_store_app/screens/cart/CartFragment.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/cart/CartFragment.kt
@@ -6,21 +6,47 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.hardware_store_app.OnItemClick
 import com.example.hardware_store_app.R
+import com.example.hardware_store_app.adapters.CartAdapter
+import com.example.hardware_store_app.databinding.FragmentCartBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class CartFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = CartFragment()
-    }
-
-    private lateinit var viewModel: CartViewModel
+    private lateinit var binding: FragmentCartBinding
+    private val viewModel by viewModels<CartViewModel>()
+    private lateinit var cartAdapter: CartAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    ): View {
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+
+        viewModel.getProductsFromCart()
+
+        initObservers()
+
+        return binding.root
     }
 
+    private fun initObservers() {
+        cartAdapter = CartAdapter(mutableListOf(), object : OnItemClick {
+            override fun onItemClick(id: String) {
+                viewModel.deleteProductFromCart(id.toInt())
+            }
+
+        })
+
+        binding.apply {
+            viewModel.cartList.observe(viewLifecycleOwner) {
+                cartAdapter.updateList(it)
+                rvCartItems.adapter = cartAdapter
+                rvCartItems.layoutManager = LinearLayoutManager(requireContext())
+            }
+        }
+    }
 }
