cb8185f09ab975e39c62d45afd6ad128649ab107
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:36:59 +0300

    Dao interface for table 'cart' added.

---
+++ app/src/main/java/com/example/hardware_store_app/db/dao/CartDao.kt
@@ -0,0 +1,19 @@
+package com.example.hardware_store_app.db.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.hardware_store_app.db.data.Cart
+
+@Dao
+interface CartDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun addProductToCart(cart: Cart)
+
+    @Query("SELECT * FROM cart")
+    suspend fun getProductsFromCart(): MutableList<Cart>
+
+    @Query("DELETE FROM cart WHERE id=:id")
+    suspend fun deleteProductFromCart(id: Int)
+}
