3fa6db61a101c0ef1d2b83d427932a5f0edbcc85
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:40:50 +0300

    List of liked items added.

--- app/src/main/java/com/example/hardware_store_app/screens/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/favorites/FavoritesFragment.kt
@@ -6,24 +6,52 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.hardware_store_app.OnItemClick
 import com.example.hardware_store_app.R
+import com.example.hardware_store_app.adapters.ProductAdapter
 import com.example.hardware_store_app.databinding.FragmentFavoritesBinding
 import com.example.hardware_store_app.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class FavoritesFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = FavoritesFragment()
-    }
     private lateinit var binding: FragmentFavoritesBinding
-    private lateinit var viewModel: FavoritesViewModel
+    private val viewModel by viewModels<FavoritesViewModel>()
+    private lateinit var productAdapter: ProductAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentFavoritesBinding.inflate(inflater, container, false)
+
+        viewModel.getLikedItems()
+
+        initObservers()
+
         return binding.root
+    }
+
+    private fun initObservers() {
+        binding.apply {
+            productAdapter = ProductAdapter(listOf(), object : OnItemClick {
+                override fun onItemClick(id: String) {
+                    findNavController().navigate(
+                        FavoritesFragmentDirections.actionFavoritesFragmentToDetailsFragment(id)
+                    )
+                }
+            })
+
+            viewModel.listOfLikedItems.observe(viewLifecycleOwner) {
+                productAdapter.updateList(it)
+                rvLikes.layoutManager = GridLayoutManager(requireContext(), 2)
+                rvLikes.adapter = productAdapter
+            }
+
+        }
     }
 
 }
