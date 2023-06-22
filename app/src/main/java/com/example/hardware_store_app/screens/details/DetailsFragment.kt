a5aedc079bbfc6ec4ea4771e3b5e44ba05dda8d6
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-22 14:38:38 +0300

    Like & add to cart buttons added.

--- app/src/main/java/com/example/hardware_store_app/screens/details/DetailsFragment.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/details/DetailsFragment.kt
@@ -7,7 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import com.example.hardware_store_app.OnItemClick
+import com.example.hardware_store_app.adapters.ProductAdapter
 import com.example.hardware_store_app.databinding.FragmentDetailsBinding
+import com.example.hardware_store_app.db.data.Like
+import com.like.LikeButton
+import com.like.OnLikeListener
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -23,10 +28,25 @@ class DetailsFragment : Fragment() {
         binding = FragmentDetailsBinding.inflate(inflater, container, false)
 
         viewModel.getProductById(args.id)
+        viewModel.isLiked(args.id)
+
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
 
+        binding.btnAddToFav.setOnLikeListener(object : OnLikeListener {
+            override fun liked(likeButton: LikeButton?) {
+                viewModel.insertProduct(Like(args.id))
+            }
+
+            override fun unLiked(likeButton: LikeButton?) {
+                viewModel.deleteProduct(args.id)
+            }
+        })
+
+        binding.btnAddToCart.setOnClickListener {
+            viewModel.addProductToCart()
+        }
+
         return binding.root
     }
-
 }
