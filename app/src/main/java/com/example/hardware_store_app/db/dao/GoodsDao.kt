35f674bfd125ec1da9f6f7019c5a592a73c25daf
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-25 01:42:43 +0300

    New table queries added.

--- app/src/main/java/com/example/hardware_store_app/db/dao/GoodsDao.kt
+++ app/src/main/java/com/example/hardware_store_app/db/dao/GoodsDao.kt
@@ -11,6 +11,18 @@ interface GoodsDao {
     @Query("SELECT * FROM goods WHERE rate = 5")
     suspend fun getPopularProducts(): List<Goods>
 
+    @Query("SELECT * FROM goods")
+    suspend fun getAllProducts(): List<Goods>
+
+    @Query("SELECT * FROM goods WHERE id=:id")
+    suspend fun getProductById(id: String): Goods
+
+    @Query("SELECT * FROM goods WHERE name LIKE '%' || :name  || '%'")
+    suspend fun getProductsByName(name: String): List<Goods>
+
+    @Query("SELECT * FROM goods WHERE category=:category")
+    suspend fun getProductsByCategory(category: String): List<Goods>
+
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertProductIntoDB(goods: Goods)
 
