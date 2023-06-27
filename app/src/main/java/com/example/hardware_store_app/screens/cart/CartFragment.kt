7be51a01f65091e50d2676c1d788874be7532931
Oleg Mukha <oleg.mukha.2002@gmail.com>
2023-06-27 21:20:41 +0300

    CartFragment updated: added new features.

--- app/src/main/java/com/example/hardware_store_app/screens/cart/CartFragment.kt
+++ app/src/main/java/com/example/hardware_store_app/screens/cart/CartFragment.kt
@@ -1,17 +1,19 @@
 package com.example.hardware_store_app.screens.cart
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.bumptech.glide.Glide
 import com.example.hardware_store_app.OnItemClick
 import com.example.hardware_store_app.R
 import com.example.hardware_store_app.adapters.CartAdapter
 import com.example.hardware_store_app.databinding.FragmentCartBinding
+import com.example.hardware_store_app.screens.favorites.FavoritesFragmentDirections
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -34,23 +36,42 @@ class CartFragment : Fragment() {
     }
 
     private fun initObservers() {
-        cartAdapter = CartAdapter(mutableListOf(), object : OnItemClick {
-            override fun onItemClick(id: String) {
-                viewModel.deleteProductFromCart(id.toInt())
-            }
-
-        })
-
+        var orderPrice = 0.0
         binding.apply {
+            cartAdapter = CartAdapter(mutableListOf(), object : OnItemClick {
+                override fun onItemClick(id: String) {
+                    viewModel.deleteProductFromCart(id.toInt())
+                }
+            })
+
             viewModel.cartList.observe(viewLifecycleOwner) {
-                var orderPrice = 0.0
 
                 it.forEach { cart -> orderPrice += cart.price ?: 0.0 }
                 tvCartPrice.text = "₴${"%.2f".format(orderPrice)}"
 
-                cartAdapter.updateList(it)
-                rvCartItems.adapter = cartAdapter
-                rvCartItems.layoutManager = LinearLayoutManager(requireContext())
+                if (it.isEmpty()) {
+                    Glide.with(requireActivity()).load(R.drawable.cart_gif).into(ivCartGif)
+                    ivCartGif.visibility = View.VISIBLE
+                    tvCartGuide.visibility = View.VISIBLE
+                    dividerCart1.visibility = View.INVISIBLE
+                    tvCartTotal.visibility = View.INVISIBLE
+                    tvCartPrice.visibility = View.INVISIBLE
+                    tvCartShipping.visibility = View.INVISIBLE
+                    btnCreateOrder.visibility = View.INVISIBLE
+                } else {
+                    cartAdapter.updateList(it)
+                    rvCartItems.adapter = cartAdapter
+                    rvCartItems.layoutManager = LinearLayoutManager(requireContext())
+                }
+            }
+
+            btnCreateOrder.setOnClickListener {
+
+                viewModel.deleteAllProductsFromCart()
+
+                findNavController().navigate(
+                    CartFragmentDirections.actionCartFragmentToOrderFragment()
+                )
             }
         }
     }
